5015d75a44a540ccf3aeb1049aee714f84df339e
Mohd Asif <38999996+gubali@users.noreply.github.com>
2024-04-08 11:28:25 +0530

    react social media implementaion

---
+++ social-media/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Header from './components/Header';
+import Footer from './components/Header';
+import SideBar from './components/SideBar';
+import "bootstrap/dist/css/bootstrap.min.css"
+function App() {
+  return (
+    <>
+ <Header></Header>
+ <SideBar></SideBar>
+ <Footer></Footer>
+    </>
+  )
+}
+
+export default App
